655021614a29c4f9eca70d117987b6df74562df6
edsnasc <edsnascimento89@gmail.com>
2020-11-25 08:55:28 -0300

    add project

---
+++ README.md
@@ -0,0 +1,38 @@
+<p>
+  <h1 align="center">Calculator</h1>
+</p>
+
+# Layout 🛰
+
+<p align="center">
+  <img alt="Calculator" src=https://media.giphy.com/media/hGWDGkpFhr6tHN077E/giphy.gif>
+</p>
+
+## 🚀 Technology
+
+This project was developed with the following technologies & Tools:
+
+- [Vue Js](https://vuejs.org/)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+      <img src="./img/vuelogo.png" alt="logo" height="100px">
+      <h1>Calculadora</h1>
+      <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+
+export default {
+    components: { Calculator }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+*{
+    font-family: "RobotoMono", monospace;
+}
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(31, 64, 55), rgb(153, 242, 200));
+}
+</style>
